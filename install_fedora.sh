7cc73c0ef79a2bad1c5696a0e1e6bd23e66c3a08
Alex <alexprengere@gmail.com>
2013-11-26 15:39:36 +0100

    Structure installation script

--- install_fedora.sh
+++ install_fedora.sh
@@ -1,1 +1,7 @@
-sudo apt-get install vim screen zsh htop colordiff openssh-server python-pip ipython git subversion gcc-c++ python-devel
+sudo yum install \
+    vim screen zsh htop colordiff \
+    openssh-server \
+    python-pip ipython \
+    git subversion \
+    gcc-c++ python-devel \
+    pylint jsl
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,1 +1,7 @@
-sudo apt-get install vim screen zsh htop colordiff openssh-server python-pip ipython git subversion g++ python-dev
+sudo apt-get install \
+    vim screen zsh htop colordiff \
+    openssh-server \
+    python-pip ipython
+    git subversion \
+    g++ python-dev \
+    pylint
