cf9dfc188015c8be2e72fda445b048662719eb56
Alex <alexprengere@gmail.com>
2013-11-28 16:08:05 +0100

    Add comments to install script to explain what to do next.

--- install_fedora.sh
+++ install_fedora.sh
@@ -1,4 +1,8 @@
-sudo yum install \
+#!/bin/bash
+
+# Install common libs
+sudo yum update
+sudo yum install                  \
     vim screen zsh htop colordiff \
     openssh-server                \
     python-pip ipython            \
@@ -6,3 +10,9 @@ sudo yum install \
     gcc-c++ python-devel          \
     pylint jsl                    \
     nfs-utils nfs-utils-lib
+
+# Changing default shell
+#chsh -s /bin/zsh
+
+# Generating keys
+#ssh-keygen -t rsa -b 2048
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,4 +1,8 @@
-sudo apt-get install \
+#!/bin/bash
+
+# Install common libs
+sudo apt-get update
+sudo apt-get install              \
     vim screen zsh htop colordiff \
     openssh-server                \
     python-pip ipython            \
@@ -6,3 +10,9 @@ sudo apt-get install \
     g++ python-dev                \
     pylint                        \
     nfs-common
+
+# Changing default shell
+#chsh -s /bin/zsh
+
+# Generating keys
+#ssh-keygen -t rsa -b 2048
