00f0ff7978d4d7a978ae077cf35b1ebf7d7db0ba
Alex <alexprengere@gmail.com>
2025-08-27 16:20:53 +0200

    Update liquid.ps1

--- liquid.ps1
+++ liquid.ps1
@@ -1,29 +1,14 @@
-
-#######################################
-# LIQUID PROMPT EXAMPLE TEMPLATE FILE #
-#######################################
-
-# This file is not updated with new template sections. This is an example of
-# how to make a template, not what can go in one.
-# Available sections can be found in the documentation:
-# https://liquidprompt.readthedocs.io/en/stable/theme/default.html#templates
-
-# Remember that most features come with their corresponding colors.
-
-# add time, jobs, load and battery
-LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_TEMP}${LP_JOBS}"
-# add user, host and permissions colon
+LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_TEMP}${LP_RAM}${LP_DISK}${LP_WIFI}${LP_JOBS}"
 LP_PS1+="${LP_USER}${LP_HOST}${LP_PERM} "
-
-LP_PS1+="${LP_PWD}${LP_DIRSTACK}${LP_SCLS}${LP_VENV}${LP_PROXY}"
+LP_PS1+="${LP_PWD}${LP_DIRSTACK}${LP_PROXY}${LP_ENVVARS}${LP_SHLVL}${LP_DEV_ENV}"
 
 # Add VCS infos
 LP_PS1+="${LP_VCS}"
 
-# add return code and prompt mark
-LP_PS1+="${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}${LP_PS1_POSTFIX}"
+# Add last runtime, return code & meaning, prompt mark and user-defined postfix.
+LP_PS1+="${LP_RUNTIME}${LP_ERR}${LP_ERR_MEANING}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}${LP_PS1_POSTFIX}"
 
-# Get the current prompt on the fly and make it a title
+# Get the core sections without prompt escapes and make them into a title.
 _lp_formatted_title "$LP_PS1"
 
 # vim: set et sts=4 sw=4 tw=120 ft=sh:
